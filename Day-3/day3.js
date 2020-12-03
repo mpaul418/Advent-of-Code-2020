94894fc65e1cf3c71a4c8dc42dc7e201509dcef1
Michael McGillicuddy <mpm0418@gmail.com>
2020-12-03 12:48:41 -0500

    minor updates to day 3

--- Day-3/day3.js
+++ Day-3/day3.js
@@ -1,12 +1,12 @@
 const fs = require('fs');
 
 fs.readFile('./Day-3/input.txt', 'utf8', (err, data) => {
-    let lines = data.split('\n')
+    const lines = data.split('\n')
     lines.pop()
 
     const slopeTreesHit = (deltaX, deltaY) => {
         let x = 0
-        let maxX = lines[0].length
+        const maxX = lines[0].length
         let y = 0
         let treesHit = 0
     
@@ -22,7 +22,8 @@ fs.readFile('./Day-3/input.txt', 'utf8', (err, data) => {
     }
     
     const reducer = (accumulator, currentValue) => accumulator * currentValue;
-    let slopeTreeCounts = [slopeTreesHit(1,1), slopeTreesHit(3,1), slopeTreesHit(5,1), slopeTreesHit(7,1), slopeTreesHit(1,2)]
+    const slopeTreeCounts = [slopeTreesHit(1,1), slopeTreesHit(3,1), slopeTreesHit(5,1), slopeTreesHit(7,1), slopeTreesHit(1,2)]
 
+    console.log(`Part 1 trees hit: ${slopeTreeCounts[1]}`)
     console.log(`Product of all trees hit: ${slopeTreeCounts.reduce(reducer)}`)
 })
