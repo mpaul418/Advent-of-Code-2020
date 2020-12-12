efe1abbc635c5364c5be3eff79d3804ec2570e61
Michael McGillicuddy <mpm0418@gmail.com>
2020-12-12 14:19:31 -0500

    cleaned up day 12 code

--- Day-12/day12.js
+++ Day-12/day12.js
@@ -13,22 +13,26 @@ fs.readFile('./Day-12/input.txt', 'utf8', (err, data) => {
     directionMap.set('E', east)
     directionMap.set('W', west)
 
-    const absoluteDirection = (direction, current, distance) => {
+    const updateLocation = (direction, currentDirection, currentX, currentY, distance) => {
+        if(direction === 'F')
+            direction = currentDirection
+
         switch(direction) {
             case 'N':
+                return [currentX, currentY + distance, currentDirection]
             case 'S':
+                return [currentX, currentY - distance, currentDirection]
             case 'E':
+                return [currentX + distance, currentY, currentDirection]
             case 'W':
-                return [direction, current, distance]
-            case 'F':
-                return [current, current, distance]
+                return [currentX - distance, currentY, currentDirection]
             default: // on L or R
-                const directionObject = directionMap.get(current)
+                const directionObject = directionMap.get(currentDirection)
                 if(distance === 180)
-                    return [directionObject.reverse, directionObject.reverse, 0]
+                    return [currentX, currentY, directionObject.reverse]
                 if(distance === 270)
                     direction = direction === 'L' ? 'R' : 'L' // switches a 270 degree rotation to 90 degrees the opposite direction
-                return direction === 'L' ? [directionObject.left, directionObject.left, 0] : [directionObject.right, directionObject.right, 0]
+                return direction === 'L' ? [currentX, currentY, directionObject.left] : [currentX, currentY, directionObject.right]
         }
     }
 
@@ -48,49 +52,34 @@ fs.readFile('./Day-12/input.txt', 'utf8', (err, data) => {
                 if(distance === 270)
                     direction = direction === 'L' ? 'R' : 'L' // switches a 270 degree rotation to 90 degrees the opposite direction
                 
-                return direction === 'L' ? [-currentY, currentX] : [currentY, -currentX]
+                return direction === 'L' ? [-currentY, currentX] : [currentY, -currentX] // rotates the waypoint location 90 degrees
         }
     }
 
     // part 1
     let currentDirection = 'E'
-    let travelDirection = 'E'
     let X = 0, Y = 0
     for(let line of lines) {
         let distance = parseInt(line.slice(1));
-        ([travelDirection, currentDirection, distance] = absoluteDirection(line.slice(0,1), currentDirection, distance))
-        
-        switch(travelDirection) {
-            case 'N':
-                Y += distance
-                break
-            case 'S':
-                Y -= distance
-                break
-            case 'E':
-                X += distance
-                break
-            case 'W':
-                X -= distance
-        }
+        ([X, Y, currentDirection] = updateLocation(line.slice(0,1), currentDirection, X, Y, distance))
     }
 
     console.log(`Part 1 Manhattan distance is ${Math.abs(X) + Math.abs(Y)}.`)
 
     // part 2
-    currentDirection = 'E'
+    let nextDirection
     X = 0, Y = 0
     let waypointX = 10, waypointY = 1
     for(let line of lines) {
-        currentDirection = line.slice(0,1)
+        nextDirection = line.slice(0,1)
         let distance = parseInt(line.slice(1))
-        if(currentDirection === 'F') {
+        if(nextDirection === 'F') {
             X += waypointX * distance
             Y += waypointY * distance
             continue
         }
 
-        ([waypointX, waypointY] = updateWaypoint(currentDirection, waypointX, waypointY, distance))
+        ([waypointX, waypointY] = updateWaypoint(nextDirection, waypointX, waypointY, distance))
     }
 
     console.log(`Part 2 Manhattan distance is ${Math.abs(X) + Math.abs(Y)}.`)
